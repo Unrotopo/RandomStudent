a516caac6523b09259b0396e6e05626dbaebcf38
Luis Portas <otrorotopo@gmail.com>
2025-03-25 13:23:42 +0100

    mejora nombres variables

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
 
-        RandomStudent rs = new RandomStudent();
+        RandomStudent randomStudent = new RandomStudent();
 
-        rs.getRandomStudent();
+        randomStudent.getRandomStudent();
     }
 }
--- src/RandomStudent.java
+++ src/RandomStudent.java
@@ -1,12 +1,9 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class RandomStudent {
 
-    public ArrayList<String> students = new ArrayList<>(List.of("Jofre", "Angel", "Fernando", "Antonio", "Ezequiel", "Alfonso", "Pablo", "Isaac", "Cintia",
-            "Aurélien", "Marat", "Luis", "Jose María", "Mar", "Río Hierba", "Marc", "Zohra", "Flavio Augusto", "Pablo Federico"));
+    List<String> students = Arrays.asList("Jofre", "Angel", "Fernando", "Antonio", "Ezequiel", "Alfonso", "Pablo", "Isaac", "Cintia",
+            "Aurélien", "Marat", "Luis", "Jose María", "Mar", "Río Hierba", "Marc", "Zohra", "Flavio Augusto", "Pablo Federico");
     Scanner sc = new Scanner(System.in);
 
     public void getRandomStudent() {
@@ -17,9 +14,9 @@ public class RandomStudent {
             sc.nextLine();
 
             Random rand = new Random();
-            int n = rand.nextInt(students.size());
-            String studentName = students.get(n);
-            students.remove(n);
+            int randomIndex = rand.nextInt(students.size());
+            String studentName = students.get(randomIndex);
+            students.remove(randomIndex);
 
             System.out.println("Our next victim is " + studentName + "\n");
 
