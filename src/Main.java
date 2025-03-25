fb9483192dc1a1ffe29190dd38d3ec07eadc0740
Luis Portas <otrorotopo@gmail.com>
2025-03-25 12:55:16 +0100

    quien sera el proximo

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+        RandomStudent rs = new RandomStudent();
+
+        rs.getRandomStudent();
+    }
+}
---
+++ src/RandomStudent.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class RandomStudent {
+
+    public ArrayList<String> students = new ArrayList<>(List.of("Jofre", "Angel", "Fernando", "Antonio", "Ezequiel", "Alfonso", "Pablo", "Isaac", "Cintia",
+            "Aurélien", "Marat", "Luis", "Jose María", "Mar", "Río Hierba", "Marc", "Zohra", "Flavio Augusto", "Pablo Federico"));
+    Scanner sc = new Scanner(System.in);
+
+    public void getRandomStudent() {
+
+        while (students.size() >= 0) {
+
+            System.out.println("To get a student press any key.");
+            sc.nextLine();
+
+            Random rand = new Random();
+            int n = rand.nextInt(students.size());
+            String studentName = students.get(n);
+            students.remove(n);
+
+            System.out.println("Our next victim is " + studentName + "\n");
+
+        }
+    }
+}
